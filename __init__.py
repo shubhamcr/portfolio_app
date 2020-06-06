f42f1a163ab9b445b1fa2af05c6a1cb9671ef4a7
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-06 18:22:40 +0530

    Added flask-sqlqlchemy and flask-migrate.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     return app
 
-from portfolio import views
+
+from . import views
--- portfolio.py
+++ portfolio.py
@@ -1,3 +1,9 @@
-from . import create_app
+from . import create_app, db
+from .models import contact_message
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'ContactMessage': contact_message.ContactMessage}
