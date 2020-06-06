e6dffc6fe9965a34c35f4d50ee043c92883d623a
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-06 11:43:58 +0530

    Refactored skills section to use jinja templating. Changed styling of skills section.

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from .portfolio import app
+from portfolio import profile
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', skills = profile.get_skills())
