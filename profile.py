b0cdc9d21ad81773368abfcaa5df97f441b0f32b
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-06 11:41:38 +0530

    Added skills list in profile.py.

---
+++ profile.py
@@ -0,0 +1,4 @@
+def get_skills():
+    return ["C++", "Python", "Java", "Flask", "Qt", "Spring", "Html",
+            "CSS", "Bootstrap", "Sql", "pytest", "Data Structures",
+            "Algorithms", "Machine Learning", "Git"]
