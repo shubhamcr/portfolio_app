a3f92b0556e2cd70a1757f6f13f86762d9b586e5
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-06 18:21:01 +0530

    Added datetime and tablename attribute in ContactMessage model.

--- models/contact_message.py
+++ models/contact_message.py
@@ -1,11 +1,15 @@
 from .. import db
+from sqlalchemy import func
 
 
 class ContactMessage(db.Model):
+    __tablename__ = "ContactMessage"
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), index=True)
     subject = db.Column(db.String(), index=True)
     content = db.Column(db.String())
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
-        return f'Email: {self.email}\nSubject: {self.subject}\nContent: {self.content}'
+        return f'Email: {self.email}\nSubject: {self.subject}\nContent: {self.content}\nTime: {self.time_created}'
