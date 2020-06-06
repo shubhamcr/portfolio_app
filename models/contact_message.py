6724b81026d3d48a6f668dfbea039a558de17eba
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-06 18:31:03 +0530

    Made ContactMessage table columns non-nullable.

--- models/contact_message.py
+++ models/contact_message.py
@@ -6,9 +6,9 @@ class ContactMessage(db.Model):
     __tablename__ = "ContactMessage"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(), index=True)
-    subject = db.Column(db.String(), index=True)
-    content = db.Column(db.String())
+    email = db.Column(db.String(), index=True, nullable=False)
+    subject = db.Column(db.String(), index=True, nullable=False)
+    content = db.Column(db.String(), nullable=False)
     time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
