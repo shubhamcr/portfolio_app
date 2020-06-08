32cc62470f03991226f33e168ae8170b5ce9f810
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-08 10:59:14 +0530

    Added email and text validator.

---
+++ validators/validator.py
@@ -0,0 +1,31 @@
+from validate_email import validate_email
+
+
+def validate(validator):
+    return validator.validate()
+
+
+class Validator:
+    def __init__(self, field):
+        self.field = field
+
+    def validate(self):
+        pass
+
+
+class EmailValidator(Validator):
+    def __init__(self, email: str):
+        super(EmailValidator, self).__init__(email)
+
+    def validate(self):
+        return validate_email(email_address=self.field, check_regex=True, check_mx=True,
+                              from_address='sprasad3101@gmail.com', helo_host='gmail.com', smtp_timeout=10,
+                              dns_timeout=10, use_blacklist=True)
+
+
+class TextValidator(Validator):
+    def __init__(self, text: str):
+        super(TextValidator, self).__init__(text)
+
+    def validate(self):
+        return self.field.strip()
