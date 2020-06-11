6f9d695b56726ecbea52f631e62cc28bedc59dce
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-11 22:08:19 +0530

    Added flask mail in app.

--- __init__.py
+++ __init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_mail import Mail
 
 db = SQLAlchemy()
 migrate = Migrate()
+mail = Mail()
 
 
 def create_app():
@@ -14,6 +15,7 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    mail.init_app(app)
 
     return app
 
