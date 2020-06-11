37e976c84fc2696b2a5b2c3a36a5dedd02b0697f
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-06-11 22:09:05 +0530

    Modified contact message model.

--- models/contact_message.py
+++ models/contact_message.py
@@ -6,10 +6,15 @@ class ContactMessage(db.Model):
     __tablename__ = "ContactMessage"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(), index=True, nullable=False)
+    email_id = db.Column(db.String(), index=True, nullable=False)
     subject = db.Column(db.String(), index=True, nullable=False)
     content = db.Column(db.String(), nullable=False)
-    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    time_created = db.Column(db.DateTime(timezone=True), index=True, server_default=func.now())
+
+    def __init__(self, email_id, subject, content):
+        self.email_id = email_id
+        self.subject = subject
+        self.content = content
 
     def __repr__(self):
-        return f'Email: {self.email}\nSubject: {self.subject}\nContent: {self.content}\nTime: {self.time_created}'
+        return f'Email: {self.email_id}\nSubject: {self.subject}\nContent: {self.content}\nTime: {self.time_created}'
