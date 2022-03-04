2c11e1ad8346dbf0510caa678959d7a93c448cfc
Shubham Prasad Singh <sprasad3101@gmail.com>
2022-03-04 21:06:20 +0530

    Updated work section, profile photo and added new photos in outside work section

--- portfolio_app/profile.py
+++ portfolio_app/profile.py
@@ -1,5 +1,6 @@
 def get_skills():
-    return ["C++", "Python", "Java", "Flask", "Qt", "Spring", "Html",
+    return ["C++", "Boost", "Google Protobuf", "googletest", "Python", "Java",
+            "Flask", "Qt", "Spring", "Html",
             "CSS", "Bootstrap", "Sql", "pytest", "Data Structures",
             "Algorithms", "Machine Learning", "Git"]
 
@@ -23,8 +24,18 @@ def outside_work_images():
             "img_caption": "Group Hangouts (March 2018)"
         },
         {
+            "img_url": img_base_url + "volleyball.jpg",
+            "img_alt": "Volleyball",
+            "img_caption": "Volleyball | He who climbs the ladder must start at the bottom. (Dec 2019)"
+        },
+        {
             "img_url": img_base_url + "go_karting.jpg",
             "img_alt": "Go Karting",
             "img_caption": "Go Karting (Dec 2019)"
+        },
+        {
+            "img_url": img_base_url + "family_meeting.jpg",
+            "img_alt": "Family Meeting a.k.a. Sit-Down",
+            "img_caption": "Family Meeting a.k.a. Sit-Down (Feb 2022)"
         },
     ]
